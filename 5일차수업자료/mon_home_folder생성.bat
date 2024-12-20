9bf836a22534d4fface23201241925f2f0ebf505
kim Young Chul (김영철) <163226824+powermvp116@users.noreply.github.com>
2024-12-20 09:35:10 +0900

    Add files via upload

---
+++ 5일차수업자료/mon_home_folder생성.bat
@@ -0,0 +1,31 @@
+e:
+mkdir "ftp FTP"
+cd \"ftp FTP"
+ 
+mkdir luser71
+mkdir luser72
+mkdir luser73
+
+mkdir luser71\"이 폴더는 luser71 아래에 있습니다"
+mkdir luser72\"이 폴더는 luser72 아래에 있습니다"
+mkdir luser73\"이 폴더는 luser73 아래에 있습니다"
+
+ 
+mkdir LocalUser
+mkdir LocalUser\luser71
+mkdir LocalUser\luser72
+mkdir LocalUser\luser73
+ 
+mkdir LocalUser\luser71\"이 폴더는 LocalUser - luser71 아래에 있습니다"
+mkdir LocalUser\luser72\"이 폴더는 LocalUser - luser72 아래에 있습니다"
+mkdir LocalUser\luser71\"이 폴더는 LocalUser - luser73 아래에 있습니다"
+ 
+ 
+mkdir studykorea
+mkdir studykorea\user71
+mkdir studykorea\user72
+mkdir studykorea\user73
+ 
+mkdir studykorea\user71\"이 폴더는 studykorea - user71 아래에 있습니다"
+mkdir studykorea\user72\"이 폴더는 studykorea - user72 아래에 있습니다"
+mkdir studykorea\user73\"이 폴더는 studykorea - user73 아래에 있습니다"
